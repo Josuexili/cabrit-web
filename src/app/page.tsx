3878db4eb6320676d54eca1ee747e30bb7544be7
Josuè González Santacreu <josue.gonzalez.santacreu@gmail.com>
2025-06-05 13:11:02 +0200

    casi casi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Menu, X } from 'lucide-react'
 import FooterLegal from './components/FooterLegal'
 import Carousel from './components/Carru'
